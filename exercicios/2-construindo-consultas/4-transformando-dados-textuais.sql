6b0dcedfd9e375bf388a5f676406526fae6daf37
Victor Colen <83655407+vcolen@users.noreply.github.com>
2024-04-15 19:25:48 +0000

    2.4

--- exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql
+++ exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql
@@ -1,16 +1,56 @@
 -- Crie uma consulta que retorne as colunas CustomerId, FirstName, LastName e Address das pessoas consumidoras brasileiras
-
+SELECT DISTINCT
+CustomerId as id,
+FirstName as nome,
+LastName as sobrenome,
+Address as endereco
+FROM customers
+WHERE Country LIKE 'Bra%'
 
 -- Na consulta anterior, converta o sobrenome para letras maiúsculas
-
+SELECT DISTINCT
+CustomerId as id,
+FirstName as nome,
+UPPER(LastName) as sobrenome,
+Address as endereco
+FROM customers
+WHERE Country LIKE 'Bra%'
 
 -- Na consulta anterior, converta o nome para letras minúsculas
-
+SELECT DISTINCT
+CustomerId as id,
+LOWER(FirstName) as nome,
+UPPER(LastName) as sobrenome,
+Address as endereco
+FROM customers
+WHERE Country LIKE 'Bra%'
 
 -- Na consulta anterior, adicione uma coluna cujos os dados seja o resultado da concatenação das colunas de nome e sobrenome
-
+SELECT DISTINCT
+CustomerId as id,
+LOWER(FirstName) as nome,
+UPPER(LastName) as sobrenome,
+FirstName || ' ' || LastName as nome_completo,
+Address as endereco
+FROM customers
+WHERE Country LIKE 'Bra%'
 
 -- Na consulta anterior, substitua "Av." por "Avenida" na coluna endereço
-
+SELECT DISTINCT
+CustomerId as id,
+LOWER(FirstName) as nome,
+UPPER(LastName) as sobrenome,
+FirstName || ' ' || LastName as nome_completo,
+REPLACE (Address, 'Av.', 'Avenida') as endereco
+FROM customers
+WHERE Country LIKE 'Bra%'
 
 -- Na consulta anterior, utilize o símbolo de coringa para construir padrões textuais
+SELECT DISTINCT
+CustomerId as id,
+LOWER(FirstName) as nome,
+UPPER(LastName) as sobrenome,
+FirstName || ' ' || LastName as nome_completo,
+REPLACE (Address, 'Av.', 'Avenida') as endereco
+FROM customers
+WHERE Country LIKE 'Bra%'
