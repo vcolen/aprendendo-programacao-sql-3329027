34c27588cba03a2af36b39b93df350718fd32d8c
Victor Colen <83655407+vcolen@users.noreply.github.com>
2024-04-15 20:14:48 +0000

    4.1

--- exercicios/4-refinando-consulta/1-filtro.sql
+++ exercicios/4-refinando-consulta/1-filtro.sql
@@ -1,1 +1,13 @@
 -- Crie uma consulta para realizar inner join com as tabelas "tracks", "albums" e "artists". Além disso, apresente apenas as músicas do artista que contenha "Nação" no nome e título do álbum que não seja "Da Lama Ao Caos"
+SELECT 
+tra.TrackId AS id,
+tra.Name AS musica,
+alb.Title AS album,
+art.Name AS artista
+FROM 
+tracks AS tra
+INNER JOIN albums AS alb ON tra.AlbumId = alb.AlbumId
+INNER JOIN artists AS art ON art.ArtistId = alb.ArtistId
+WHERE
+artista LIKE '%Nação%'
+AND album NOT LIKE 'Da Lama ao Caos'; 
